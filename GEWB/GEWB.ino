a05748fd234daef8dd99578df2fe1ff7b75ec252
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-04-20 18:36:26 +0200

    init

--- GEWB/GEWB.ino
+++ GEWB/GEWB.ino
@@ -5,6 +5,9 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
+
+static const int ID = 1234;
+
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
